ac6bf3b4d2f7fe64e71c1178fe3e8716a9f05b8f
Ярослав WaDss <wadss07@mail.ru>
2023-09-13 22:19:26 +0300

    code according to PEP8

--- main.py
+++ main.py
@@ -23,12 +23,13 @@ def defence(char_name, char_class):
 
 
 def special(char_name, char_class):
-    if char_class == 'warrior': 
-        return (f'{char_name} применил специальное умение «Выносливость 105»')
+    if char_class == 'warrior':
+        return (f'{char_name} применил специальное умение '
+                f'«Выносливость {80 + 25}»')
     if char_class == 'mage':
-        return (f'{char_name} применил специальное умение «Атака 45»')
+        return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
     if char_class == 'healer':
-        return (f'{char_name} применил специальное умение «Защита 40»')
+        return (f'{char_name} применил специальное умение «Защита {10 + 30}»')
 
 
 def start_training(char_name, char_class):
